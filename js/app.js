7e8c757319840ac7b46e13123081da107e320e0b
Daniel Jordão <101356855+dsjordao@users.noreply.github.com>
2022-10-30 15:29:44 -0300

    Atualizando JS

--- js/app.js
+++ js/app.js
@@ -1,40 +1,13 @@
 const visualizar =[
     {
         id: 1,
-        nome: "susan smith",
+        nome: "daniel jordao",
         funcao: "web developer",
         img:
           "./assets/img/",
         texto:
-          "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-      },
-      {
-        id: 2,
-        nome: "anna johnson",
-        funcao: "web designer",
-        img:
-          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-        texto:
-          "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-      },
-      {
-        id: 3,
-        nome: "peter jones",
-        funcao: "intern",
-        img:
-          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-        texto:
-          "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-      },
-      {
-        id: 4,
-        nome: "bill anderson",
-        funcao: "the boss",
-        img:
-          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-        texto:
-          "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-      },
+          "work in Progress",
+      }
     ];
 const img = document.getElementById("pessoa-img");
 const autor = document.getElementById("autor");
